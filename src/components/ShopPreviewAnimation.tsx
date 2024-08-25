84054d55568bc83da2197438f1bfa378e1b470d3
goshamartynovich <goshamartynovich@gmail.com>
2024-08-25 22:09:29 +0400

    feat: + shopPreviewAnimation

---
+++ src/components/ShopPreviewAnimation.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import { motion } from "framer-motion";
+import { AnimatePresence } from "framer-motion";
+import { ShopCard } from "./ShopCard";
+import { ShopItem } from "@/types";
+
+interface ShopPreviewAnimationProps {
+  products: ShopItem[];
+}
+
+const ShopPreviewAnimation = ({ products }: ShopPreviewAnimationProps) => {
+  const showAnimation = true;
+
+  return (
+    <AnimatePresence>
+      {products.map((product, index) => (
+        <motion.div
+          key={product.name}
+          initial={showAnimation ? { y: 10, opacity: 0 } : {}}
+          animate={showAnimation ? { y: 0, opacity: 1 } : {}}
+          exit={showAnimation ? { y: -10, opacity: 0 } : {}}
+          transition={{ duration: 0.4, delay: index * 0.1 }}
+        >
+          <ShopCard key={product.parentProductId} product={product} />
+        </motion.div>
+      ))}
+    </AnimatePresence>
+  );
+};
+
+export { ShopPreviewAnimation };

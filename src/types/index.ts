39f53dfd8850354887d75547f5df1b9fe32f185f
goshamartynovich <goshamartynovich@gmail.com>
2024-08-25 20:17:03 +0400

    feat: + product types

---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export {
+  type GetProductsResponse,
+  type Product,
+  type Offer,
+  type OfferPrice,
+  type ProductGroup,
+  type Pagination,
+  type ShopItem,
+} from "./product";
---
+++ src/types/product.ts
@@ -0,0 +1,79 @@
+export interface ProductGroup {
+  id: number;
+}
+
+export interface OptionValue {
+  value: string;
+  default: boolean;
+}
+
+export interface Option {
+  code: string;
+  values: OptionValue[];
+}
+
+export interface OfferPrice {
+  priceType: string;
+  price: number;
+  ordering: number;
+  currency: string;
+}
+
+export interface Offer {
+  name: string;
+  price: number;
+  images: string[];
+  id: number;
+  prices: OfferPrice[];
+  purchasePrice: number;
+  vatRate: string;
+  properties: { [key: string]: string };
+  quantity: number;
+  active: boolean;
+}
+
+export interface Pagination {
+  limit: number;
+  totalCount: number;
+  currentPage: number;
+  totalPageCount: number;
+}
+
+export interface Product {
+  type: string;
+  minPrice: number;
+  maxPrice: number;
+  catalogId: number;
+  id: number;
+  name: string;
+  imageUrl: string;
+  description: string;
+  popular: boolean;
+  stock: boolean;
+  novelty: boolean;
+  recommended: boolean;
+  options?: Option[];
+  groups: ProductGroup[];
+  manufacturer: string;
+  offers: Offer[];
+  updatedAt: string;
+  active: boolean;
+  quantity: number;
+  markable: boolean;
+}
+
+export interface ShopItem {
+  name: string;
+  imgs: string[];
+  parentProductId: number;
+  isOutOfStock: boolean;
+  price: number;
+  description: string;
+  color?: string;
+}
+
+export interface GetProductsResponse {
+  success: boolean;
+  pagination: Pagination;
+  products: Product[];
+}

44bd138484d9bfd5ca9af6e57aa128ffeaa0c24a
goshamartynovich <goshamartynovich@gmail.com>
2024-08-25 22:06:34 +0400

    feat: + shop page

--- src/app/shop/page.tsx
+++ src/app/shop/page.tsx
@@ -1,9 +1,95 @@
 import { BaseLayout } from "@/layouts/BaseLayout";
+import { Product, GetProductsResponse, ShopItem } from "@/types";
 
-const ShopPage = () => {
+import { ShopCard } from "@/components";
+
+const transformAllProductsData = (products: Product[]): { transformedProducts: ShopItem[] } => {
+  const transformedProducts: ShopItem[] = [];
+
+  products.forEach((product) => {
+    const colorMap: { [key: string]: ShopItem } = {};
+    let noColorProduct: ShopItem | null = null;
+
+    product.offers.forEach((offer) => {
+      const color = offer.properties?.cvet;
+      const size = offer.properties?.razmer;
+
+      if (!color) {
+        if (!noColorProduct) {
+          noColorProduct = {
+            name: product.name,
+            imgs: [...offer.images],
+            parentProductId: product.id,
+            price: product.minPrice,
+            isOutOfStock: offer.quantity === 0,
+            description: product.description,
+            color: color,
+          };
+        } else {
+          noColorProduct.isOutOfStock = noColorProduct.isOutOfStock && offer.quantity === 0;
+
+          // Manually add unique images to the array
+          offer.images.forEach((img) => {
+            if (!noColorProduct!.imgs.includes(img)) {
+              noColorProduct!.imgs.push(img);
+            }
+          });
+        }
+      } else {
+        if (!colorMap[color]) {
+          colorMap[color] = {
+            name: `${product.name} ${color}`,
+            imgs: [...offer.images],
+            parentProductId: product.id,
+            price: offer.price,
+            isOutOfStock: true,
+            description: product.description,
+            color: color,
+          };
+        }
+
+        if (offer.quantity > 0) {
+          colorMap[color].isOutOfStock = false;
+        }
+      }
+    });
+
+    if (noColorProduct) {
+      transformedProducts.push(noColorProduct);
+    }
+
+    Object.values(colorMap).forEach((colorProduct) => {
+      transformedProducts.push(colorProduct);
+    });
+  });
+
+  return { transformedProducts };
+};
+
+const fetchProducts = async (): Promise<ShopItem[]> => {
+  const response = await fetch(`${process.env.NEXT_PUBLIC_SITE_URL}/api/getProducts`, {
+    cache: "force-cache", // This ensures the data is fetched during build time
+  });
+
+  if (!response.ok) {
+    throw new Error("Failed to fetch products");
+  }
+
+  const data: GetProductsResponse = await response.json();
+  const { transformedProducts } = transformAllProductsData(data.products);
+  return transformedProducts;
+};
+
+const ShopPage = async () => {
+  const products = await fetchProducts();
+
   return (
     <BaseLayout>
-      <div>shop</div>
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-2 sm:gap-4 py-2 px-2 sm:py-0">
+        {products.map((product, index) => (
+          <ShopCard key={product.parentProductId} product={product} />
+        ))}
+      </div>
     </BaseLayout>
   );
 };

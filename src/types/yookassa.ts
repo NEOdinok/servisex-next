578acceaed587f034277aa07f72c62ea8d05805f
goshamartynovich <goshamartynovich@gmail.com>
2024-10-12 12:21:33 +0300

    yookassa, retailCRM types

---
+++ src/types/yookassa.ts
@@ -0,0 +1,22 @@
+export interface YookassaPaymentResponse {
+  id: string;
+  status: string;
+  paid: boolean;
+  amount: {
+    value: string;
+    currency: string;
+  };
+  confirmation: {
+    type: string;
+    confirmation_url: string;
+  };
+  created_at: string;
+  description: string;
+  metadata: Record<string, unknown>;
+  recipient: {
+    account_id: string;
+    gateway_id: string;
+  };
+  refundable: boolean;
+  test: boolean;
+}

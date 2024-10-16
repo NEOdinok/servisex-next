3f6a46ae1b0ef3cac2254234c1fb1cde9150f974
goshamartynovich <goshamartynovich@gmail.com>
2024-10-16 09:53:48 +0300

    removed more client api stuff

--- src/app/rootReducer.ts
+++
@@ -1,6 +0,0 @@
-import { clientApi } from "@/client-api";
-import { combineReducers } from "@reduxjs/toolkit";
-
-export const rootReducer = combineReducers({
-  [clientApi.reducerPath]: clientApi.reducer,
-});

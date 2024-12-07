282d4574bc14e4642dfbbbd0bb17f591477decd1
goshamartynovich <goshamartynovich@gmail.com>
2024-12-07 19:27:45 +0300

    more JSON.stringify()

--- src/app/api/yooKassaTest/route.ts
+++ src/app/api/yooKassaTest/route.ts
@@ -120,7 +120,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         },
         body: JSON.stringify({
           by: "id",
-          order: { status: "no-product" },
+          order: JSON.stringify({ status: "no-product" }),
         }),
       });
 

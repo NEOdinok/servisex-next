262436c93632cec93924b7adef493faf6416166f
goshamartynovich <goshamartynovich@gmail.com>
2024-12-09 14:18:06 +0300

    logs, order status json instead of JSON.stringify()

--- src/app/api/yooKassaTest/route.ts
+++ src/app/api/yooKassaTest/route.ts
@@ -37,19 +37,20 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const retailCrmApiKey = process.env.NEXT_PUBLIC_RETAIL_CRM_API;
     const notificationIp = request.headers.get("x-forwarded-for") || request.headers.get("client-ip");
 
-    console.log("Received notification from YooKassa:", notification);
+    console.log("[1] Received notification from YooKassa:", notification);
 
     //2. Validate IP
     if (!notificationIp || !isIpValid(notificationIp)) {
       return NextResponse.json({ error: "Forbidden: Invalid IP address" }, { status: 403 });
     }
 
-    console.log("Yookassa IP validated", notificationIp);
+    console.log("[2] Yookassa IP validated", notificationIp);
 
     //3. GET product offers in created order
     const ordersResponse = await fetch(`${API_ENDPOINT_ORDERS}?apiKey=${retailCrmApiKey}&id=${orderId}`);
     const orderProductsData: GetOrdersResponse = await ordersResponse.json();
     const order = orderProductsData.orders[0];
+
     const orderProducts = order?.items;
     const deliveryPrice = order.delivery.cost;
     const productsPrice = order.summ;
@@ -61,15 +62,16 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
     const delivery = order.delivery.code || "";
 
     const offersInCreatedOrder = orderProducts.map((product) => product.offer.id);
-    console.log("offers in order:", offersInCreatedOrder);
+    console.log("[3] Offers in order:", offersInCreatedOrder);
 
-    //3. GET all products
+    //4. GET all products
     const productsResponse = await fetch(`${API_ENDPOINT_PRODUCTS}?apiKey=${retailCrmApiKey}`);
     const productsData: GetProductsResponse = await productsResponse.json();
     const products = productsData.products;
 
-    //4. Create a map of offer IDs to their quantities
-    // check for out of stock offers
+    console.log("[4] Get all products");
+
+    //5. Create a map of offer IDs to their quantities check for out of stock offers
     const offerQuantityMap = new Map<number, number>();
     const outOfStockOffers: number[] = [];
 
@@ -79,7 +81,9 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
       });
     });
 
-    // Check the quantities of the offers in the created order
+    console.log("[5] Create a map of offer - quantity:", offerQuantityMap);
+
+    //6. Check the quantities of the offers in the created order
     offersInCreatedOrder.forEach((offerId) => {
       const quantity = offerQuantityMap.get(offerId);
 
@@ -87,6 +91,10 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         outOfStockOffers.push(offerId);
       }
     });
+
+    console.log("[6] Here's what's out of slock:", outOfStockOffers);
+
+    //7. Capture or cancel payment
 
     if (outOfStockOffers.length) {
       const errorIdempotenceKey = uuidv4();
@@ -122,7 +130,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         },
         body: JSON.stringify({
           by: "id",
-          order: JSON.stringify({ status: "no-product" }),
+          order: { status: "no-product" },
         }),
       });
 
@@ -165,7 +173,7 @@ export async function POST(request: NextRequest): Promise<NextResponse> {
         },
         body: JSON.stringify({
           by: "id",
-          order: JSON.stringify({ status: "availability-confirmed" }),
+          order: { status: "availability-confirmed" },
         }),
       });
       if (!updateOrderResponse.ok) {
